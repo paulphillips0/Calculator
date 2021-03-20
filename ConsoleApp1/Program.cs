e50eb54e709379005b5488b67244c91b575bf76a
engineer_dude_3 <paulphillips@phillipsengineer.com>
2021-03-19 20:40:50 -0700

    Simplified the unit tests

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,29 +16,4 @@ namespace ConsoleApp1
             }
         }
     }
-
-    public static class ContainerConfig
-    {
-        public static IContainer Config()
-        {
-            var builder = new ContainerBuilder();
-
-            builder.RegisterType<Calculator>().As<ICalculator>();
-
-            return builder.Build();
-        }
-    }
-
-    public interface ICalculator
-    {
-        double Add(double x, double y);
-    }
-
-    public class Calculator : ICalculator
-    {
-        public double Add(double x, double y)
-        {
-            return x + y;
-        }
-    }
 }
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,26 +1,32 @@
 using System;
 using Autofac.Extras.Moq;
 using ConsoleApp1;
+using Moq;
+using Moq.Language.Flow;
 using Xunit;
 
 namespace TestProject1
 {
     public class UnitTest1
     {
+        private readonly ICalculator _calculator;
+
+        public UnitTest1()
+        {
+            AutoMock mock = AutoMock.GetStrict();
+
+            mock.Mock<ICalculator>()
+                .Setup(x => x.Add(1, 2))
+                .Returns(3);
+
+            _calculator = mock.Create<ICalculator>();
+        }
+
         [Fact]
         public void Test1()
         {
-            using (var mock = AutoMock.GetStrict())
-            {
-                mock.Mock<ICalculator>()
-                    .Setup(x => x.Add(1, 2))
-                    .Returns(3);
-
-                ICalculator calculator = mock.Create<ICalculator>();
-                
-                var sum = calculator.Add(1, 2);
-                Assert.Equal(expected:3, actual:sum);
-            }
+            var sum = _calculator.Add(1, 2);
+            Assert.Equal(expected: 3, actual: sum);
         }
     }
 }
