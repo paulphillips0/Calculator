d6a51d4d356855d908f602bf3954ded575d84add
engineer_dude_3 <paulphillips@phillipsengineer.com>
2021-03-19 19:06:04 -0700

    Next commit

---
+++ ConsoleApp1/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1
+{
+    public class Calculator : ICalculator
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ ConsoleApp1/ContainerConfig.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+
+namespace ConsoleApp1
+{
+    public static class ContainerConfig
+    {
+        public static IContainer Config()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Calculator>().As<ICalculator>();
+
+            return builder.Build();
+        }
+    }
+}
